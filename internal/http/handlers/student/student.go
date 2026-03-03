a55818b2f385423897029c68d8a18501beaf2444
Mrinal Agrawal <agrawalmrinal21@gmail.com>
2026-03-03 17:41:50 +0530

    handled error and made some types

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -1,10 +1,36 @@
 package student
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
 
-
+	"github.com/Mrinal-Agrawal21/student-api/internal/types"
+	"github.com/Mrinal-Agrawal21/student-api/internal/utils/response"
+)
 func NewStudentHandler() http.HandlerFunc{
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, new student handler!"))
+
+
+		var student types.Student
+
+		err := json.NewDecoder(r.Body).Decode(&student)
+		if errors.Is(err,io.EOF) {
+			response.WriteJson(w,http.StatusBadRequest , response.GeneralError(fmt.Errorf("empty body")))
+			return
+		}
+
+		if err != nil {
+			response.WriteJson(w,http.StatusBadRequest,response.GeneralError(err))
+		}
+
+		slog.Info("creating a student")
+
+		// Request Validation
+
+		response.WriteJson(w,http.StatusCreated,map[string]string {"success":"OK"})
 	}
 }
